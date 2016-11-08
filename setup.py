703d1c64ee5def4531376b02ee75dc0d4f3ca9bf
Geoffrey Spear <speargh@pitt.edu>
2016-11-08 11:00:32 -0500

    bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='pyvger',
     description='Interact with Ex Libris Voyager ILS',
-    version='0.2.0',
+    version='0.3.0',
     packages=find_packages(),
     author='Geoffrey Spear',
     author_email='geoffspear@gmail.com',
